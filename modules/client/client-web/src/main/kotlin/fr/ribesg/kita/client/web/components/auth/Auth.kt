360001edaed2183573a060e287f5b0552ee4a3f9
Zachary Grafton <zacharygrafton@primantibros.com>
2019-10-15 09:29:30 -0400

    Refactor the Auth component so that state can remain immutable.

--- modules/client/client-web/src/main/kotlin/fr/ribesg/kita/client/web/components/auth/Auth.kt
+++ modules/client/client-web/src/main/kotlin/fr/ribesg/kita/client/web/components/auth/Auth.kt
@@ -1,6 +1,5 @@
 package fr.ribesg.kita.client.web.components.auth
 
-import fr.ribesg.kita.client.web.components.auth.AuthContainer.State
 import fr.ribesg.kita.client.web.components.ui.button
 import fr.ribesg.kita.client.web.components.ui.input
 import kotlinx.css.Display
@@ -8,87 +7,67 @@ import kotlinx.css.FlexDirection
 import kotlinx.css.display
 import kotlinx.css.flexDirection
 import kotlinx.html.InputType
-import react.RBuilder
-import react.RComponent
 import react.RProps
 import react.RState
+import react.functionalComponent
+import react.useState
 import styled.css
 import styled.styledDiv
 import kotlin.browser.window
 
-fun RBuilder.auth() {
-    child(AuthContainer::class) {}
+interface AuthProps : RProps {
+  var loginButtonText: String
 }
 
-private class AuthContainer : RComponent<RProps, State>() {
+private data class State(
+  val loginInput: String,
+  val passwordInput: String,
+  val isSignUpMode: Boolean
+) : RState
 
-    private val isLoginButtonEnabled: Boolean
-        get() = state.run { loginInput.isNotEmpty() && passwordInput.isNotEmpty() }
-
-    init {
-        state = State(
-            loginInput = "",
-            passwordInput = "",
-            isSignUpMode = false
-        )
-    }
-
-    override fun RBuilder.render() = authView(
-        loginInput = state.loginInput,
-        onLoginChanged = ::onLoginChanged,
-        passwordInput = state.passwordInput,
-        onPasswordChanged = ::onPasswordChanged,
-        loginButtonText = if (state.isSignUpMode) "Signup" else "Login",
-        isLoginButtonEnabled = isLoginButtonEnabled,
-        onLoginButtonClicked = ::onLoginButtonClicked
+val AuthContainer = functionalComponent<AuthProps> { props ->
+  val (state, setState) = useState(
+    State(
+      loginInput = "",
+      passwordInput = "",
+      isSignUpMode = false
     )
+  )
 
-    private fun onLoginChanged(text: String) =
-        setState(state.copy(loginInput = text))
+  val onLoginChanged = { text: String ->
+    setState(state.copy(loginInput = text))
+  }
 
-    private fun onPasswordChanged(text: String) =
-        setState(state.copy(passwordInput = text))
+  val onPasswordChanged = { text: String ->
+    setState(state.copy(passwordInput = text))
+  }
 
-    private fun onLoginButtonClicked() {
-        val (login, password) = state
-        window.alert("login=$login, password=$password")
+  val onLoginButtonClicked = {
+    val (login, password) = state
+    window.alert("login=$login, password=$password")
+  }
+
+  val isLoginButtonEnabled = state.run { loginInput.isNotEmpty() && passwordInput.isNotEmpty() }
+
+  styledDiv {
+    css {
+      display = Display.flex
+      flexDirection = FlexDirection.column
     }
-
-    data class State(
-        val loginInput: String,
-        val passwordInput: String,
-        val isSignUpMode: Boolean
-    ) : RState
-
+    input(
+      onInputTextChanged = onLoginChanged,
+      value = state.loginInput
+    )
+    input(
+      onInputTextChanged = onPasswordChanged,
+      value = state.passwordInput,
+      type = InputType.password
+    )
+    button(
+      enabled = isLoginButtonEnabled,
+      onButtonClicked = onLoginButtonClicked,
+      text = props.loginButtonText
+    )
+  }
 }
 
-private fun RBuilder.authView(
-    loginInput: String,
-    onLoginChanged: (String) -> Unit,
-    passwordInput: String,
-    onPasswordChanged: (String) -> Unit,
-    loginButtonText: String,
-    isLoginButtonEnabled: Boolean,
-    onLoginButtonClicked: () -> Unit
-) {
-    styledDiv {
-        css {
-            display = Display.flex
-            flexDirection = FlexDirection.column
-        }
-        input(
-            onInputTextChanged = onLoginChanged,
-            value = loginInput
-        )
-        input(
-            onInputTextChanged = onPasswordChanged,
-            value = passwordInput,
-            type = InputType.password
-        )
-        button(
-            enabled = isLoginButtonEnabled,
-            onButtonClicked = onLoginButtonClicked,
-            text = loginButtonText
-        )
-    }
-}
