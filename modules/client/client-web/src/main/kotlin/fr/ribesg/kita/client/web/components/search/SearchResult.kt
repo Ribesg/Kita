a7b3447644d8d13bbfd4efb8b7e080f62f388663
Gaël Ribes <ribesg@gmail.com>
2020-02-29 19:07:24 +0100

    WIP

--- modules/client/client-web/src/main/kotlin/fr/ribesg/kita/client/web/components/search/SearchResult.kt
+++ modules/client/client-web/src/main/kotlin/fr/ribesg/kita/client/web/components/search/SearchResult.kt
@@ -1,18 +1,21 @@
 package fr.ribesg.kita.client.web.components.search
 
 import fr.ribesg.kita.common.model.SearchResponseMovie
+import kotlinx.css.Color
 import kotlinx.css.Display
 import kotlinx.css.FlexDirection
+import kotlinx.css.backgroundColor
 import kotlinx.css.display
 import kotlinx.css.em
 import kotlinx.css.flexDirection
+import kotlinx.css.flexShrink
 import kotlinx.css.height
 import kotlinx.css.padding
 import kotlinx.css.width
 import react.RBuilder
 import react.RProps
 import react.child
-import react.dom.h5
+import react.dom.h3
 import react.dom.p
 import react.functionalComponent
 import styled.css
@@ -36,12 +39,14 @@ private val SearchResultComponent = functionalComponent<SearchResultProps> { pro
         css {
             display = Display.flex
             flexDirection = FlexDirection.row
+            backgroundColor = Color.white
         }
         styledImg(src = movie.posterUrl) {
             key = movie.posterUrl ?: ""
             css {
                 width = 10.em
                 height = 15.em
+                flexShrink = .0
             }
         }
         styledDiv {
@@ -50,7 +55,7 @@ private val SearchResultComponent = functionalComponent<SearchResultProps> { pro
                 flexDirection = FlexDirection.column
                 padding = 1.em.value
             }
-            h5 {
+            h3 {
                 +movie.title
             }
             p {
